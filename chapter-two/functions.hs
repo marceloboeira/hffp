e6f1e0d2c5b61fc80820e28a6396296d62ec061d
Marcelo Boeira <me@marceloboeira.com>
2018-09-18 22:21:10 +0200

    Add the composed version of circle area
    Thanks to @glebec

--- chapter-two/functions.hs
+++ chapter-two/functions.hs
@@ -5,6 +5,5 @@ square x = x * x
 square' = flip (^) 2
 
 -- circleAreaWith x = 3.14 * square x (commented in favor of using pi from Prelude)
-piTimes = (*) pi
 circleAreaWith x = pi * square x
---TODO: why this doens't work circleAreaWith' = piTimes square'
+circleAreaWith' =  ((*) pi) . square'
