304ec168ac07a1149e675fa531a580261a69b102
Marcelo Boeira <me@marceloboeira.com>
2018-10-22 08:16:21 +0200

    Add first sections of chapter 11

--- chapters/chapter-11/exercises.md
+++ chapters/chapter-11/exercises.md
@@ -1,1 +1,19 @@
 # Chapter 11
+
+## Dog Types
+
+Given the datatypes defined in the above sections,
+
+1. Is Doggies a type constructor or a data constructor?
+2. What is the kind of Doggies?
+3. What is the kind of Doggies String?
+4. What is the type of Husky 10?
+5. What is the type of Husky (10 :: Integer)?
+6. What is the type of Mastiff "Scooby Doo"?
+7. Is DogueDeBordeaux a type constructor or a data constructor? Both?
+8. What is the type of DogueDeBordeaux? (DogueDeBordeaux a)
+9. What is the type of DogueDeBordeaux "doggie!" (DogueDeBordeaux String)
+
+## Vehicles
+
+`vehicle.hs`
---
+++ chapters/chapter-11/vehicle.hs
@@ -0,0 +1,38 @@
+-- Types
+data Price = Price Integer deriving (Eq, Show)
+data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Airline deriving(Eq, Show)
+
+-- Data
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+doge = Plane PapuAir
+
+-- Exercises
+
+-- 1. Vehicle
+
+-- 2.
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _) = True
+isPlane _ = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars = map (isCar)
+
+-- 3.
+
+getManu :: Vehicle -> Manufacturer
+getManu (Car m _) = m
+getManu _ = undefined
+
+
+-- 4. (Explain 4 with plane)
+-- Return 
+-- 5. (Add size)
