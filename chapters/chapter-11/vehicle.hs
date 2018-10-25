362de97183e85a3601183ebbbc4fa7b59c6ac996
Marcelo Boeira <me@marceloboeira.com>
2018-10-25 11:13:27 +0200

    Add missing exercises from vehicles session of chapter 11

--- chapters/chapter-11/vehicle.hs
+++ chapters/chapter-11/vehicle.hs
@@ -1,14 +1,15 @@
 -- Types
+type Size = Integer
 data Price = Price Integer deriving (Eq, Show)
 data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
 data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
-data Vehicle = Car Manufacturer Price | Plane Airline deriving(Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Airline Size deriving(Eq, Show)
 
 -- Data
 myCar = Car Mini (Price 14000)
 urCar = Car Mazda (Price 20000)
 clownCar = Car Tata (Price 7000)
-doge = Plane PapuAir
+doge = Plane PapuAir 100
 
 -- Exercises
 
@@ -20,7 +21,7 @@ isCar (Car _ _) = True
 isCar _ = False
 
 isPlane :: Vehicle -> Bool
-isPlane (Plane _) = True
+isPlane (Plane _ _) = True
 isPlane _ = False
 
 areCars :: [Vehicle] -> [Bool]
@@ -30,9 +31,8 @@ areCars = map (isCar)
 
 getManu :: Vehicle -> Manufacturer
 getManu (Car m _) = m
-getManu _ = undefined
 
 
--- 4. (Explain 4 with plane)
--- Return 
--- 5. (Add size)
+-- 4. Exception: chapters/chapter-11/vehicle.hs:32:1-21: Non-exhaustive patterns in function getManu
+--
+-- 5. The main changes are related to position and amount of attributes
