0e9a806a1bddffa43a04e1c2e620d885cd69658c
Marcelo Boeira <me@marceloboeira.com>
2018-11-09 15:09:39 +0100

    Count only incorrect guesses to declare game over
    A bit more complicated but worth attempting as an exercise is changing the game so that,
    as with normal hangman, only incorrect guesses count towards the guess limit.

--- chapters/chapter-13/hangman/src/Main.hs
+++ chapters/chapter-13/hangman/src/Main.hs
@@ -34,46 +34,47 @@ randomWord wl = do
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
 
-data Puzzle = Puzzle String [Maybe Char] [Char]
+data Puzzle = Puzzle String [Maybe Char] [Char] [Char]
 
 instance Show Puzzle where
-  show (Puzzle _ discovered guessed) =
-    (intersperse ' ' $ fmap renderPuzzleChar discovered) ++ " Guessed so far: " ++ guessed
+  show (Puzzle _ discovered correctGuess wrongGuess) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered) ++ " Guessed so far: " ++ (correctGuess ++ wrongGuess)
 
 
 freshPuzzle :: String -> Puzzle
-freshPuzzle s =  Puzzle s (map (const Nothing) s) []
+freshPuzzle s =  Puzzle s (map (const Nothing) s) [] []
 
 charInWord :: Puzzle -> Char -> Bool
-charInWord (Puzzle s _ _) c = elem c s
+charInWord (Puzzle s _ _ _) c = elem c s
 
 alreadyGuessed :: Puzzle -> Char -> Bool
-alreadyGuessed (Puzzle _ _ g) c = elem c g
+alreadyGuessed (Puzzle _ _ g w) c = elem c (g ++ w)
 
 renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar Nothing = '_'
 renderPuzzleChar (Just c) = c
 
-fillInCharacter :: Puzzle -> Char -> Puzzle
-fillInCharacter (Puzzle word filledInSoFar s) c =
-  Puzzle word newFilledInSoFar (c : s)
+fillInCharacter :: Bool -> Puzzle -> Char -> Puzzle
+fillInCharacter correct (Puzzle word filledInSoFar s s') c =
+  case correct of
+    True -> Puzzle word newFilledInSoFar (c : s) s'
+    False -> Puzzle word newFilledInSoFar s (c : s')
   where zipper guessed wordChar guessChar =
           if wordChar == guessed
           then Just wordChar
           else guessChar
-        newFilledInSoFar =
-          zipWith (zipper c) word filledInSoFar
+        newFilledInSoFar = zipWith (zipper c) word filledInSoFar
 
 gameOver :: Puzzle -> IO ()
-gameOver (Puzzle wordToGuess _ guessed) =
-  if (length guessed) >= 9 then
+gameOver (Puzzle wordToGuess _ _ mistakes) =
+  if (length mistakes) >= 7 then
     do putStrLn "You lose!"
        putStrLn $ "The word was: " ++ wordToGuess
        exitSuccess
    else return ()
 
 gameWin :: Puzzle -> IO ()
-gameWin (Puzzle _ filledInSoFar _) =
+gameWin (Puzzle _ filledInSoFar _ _) =
   if all isJust filledInSoFar then
     do putStrLn "You win!"
        exitSuccess
@@ -88,10 +89,10 @@ handleGuess puzzle guess = do
      return puzzle
     (True, _) -> do
       putStrLn "This character was in the word, filling in the word accordingly"
-      return (fillInCharacter puzzle guess)
+      return (fillInCharacter True puzzle guess)
     (False, _) -> do
       putStrLn "This character wasn't in the word, try again."
-      return (fillInCharacter puzzle guess)
+      return (fillInCharacter False puzzle guess)
 
 runGame :: Puzzle -> IO ()
 runGame puzzle = forever $ do
